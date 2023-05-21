36c74f1e0e26ade1aebc3805c4d57db25dd69de7
miguel <miguelpositivo02@gmail.com>
2023-05-21 20:18:02 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string resposta2;
+string resposta3;
+Console.WriteLine("esta se movendo?");
+string resposta1 = Console.ReadLine()!;
+if(resposta1 == "N"){
+    Console.WriteLine("Deveria?");
+     resposta2 = Console.ReadLine()!;
+if (resposta2 == "S"){
+    Console.WriteLine("Use WD-40.");
+}
+else{
+    Console.WriteLine("otimo");
+
+}
+}
+else 
+  {
+    Console.WriteLine("Deveria?");
+    resposta3 = Console.ReadLine()!;
+    if (resposta3 == "S"){
+    Console.WriteLine("otimo");
+
+    }
+    else{
+        Console.WriteLine("Use Silver Tape.");
+    }
+}
+
+   
